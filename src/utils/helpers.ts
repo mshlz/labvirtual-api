1229cae9a1cc7fb08027506513c92adeb254d17d
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-12 00:02:11 -0300

    lint escape regex string helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,9 @@
 export function escapeStringRegexp(value: string): string {
-	if (typeof value !== 'string') {
-		throw new TypeError('Expected a string');
-	}
+    if (typeof value !== 'string') {
+        throw new TypeError('Expected a string')
+    }
 
-	return value
-		.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
-		.replace(/-/g, '\\x2d');
+    return value
+        .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+        .replace(/-/g, '\\x2d')
 }
