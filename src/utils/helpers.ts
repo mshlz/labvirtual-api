e1950e7cb4ac177b81509c34dba8153e90965bc3
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-16 21:43:58 -0300

    add fromRule type helper - it transform a rule validation to an object

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,7 @@ export function escapeStringRegexp(value: string): string {
 export const isNull = (value: any) => value === null
 export const isUndefined = (value: any) => typeof value === 'undefined'
 export const isNullOrUndefined = (value: any) => typeof value === 'undefined' || value === null
+
+export type fromRule<T, K = any> = {
+    [key in keyof T]?: K
+}
