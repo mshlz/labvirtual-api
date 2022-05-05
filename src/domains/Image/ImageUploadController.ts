fed4394fff09facef66d1a42100e1a1f46c7bd59
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2022-05-04 21:49:47 -0300

    add upload img (for custom img picker)

--- src/domains/Image/ImageUploadController.ts
+++ src/domains/Image/ImageUploadController.ts
@@ -5,15 +5,27 @@ import { ImageUploadService } from './ImageUploadService'
 export class ImageUploadController {
     @Post('upload')
     public async uploadImage(@UploadedFile('file-0') data) {
-        
+
         const result = await ImageUploadService.uploadImage({ filename: data.originalname, size: data.size, buffer: data.buffer })
-        
+
         return {
             result: [{
                 url: result.url,
                 name: data.originalname,
                 size: data.size
             }]
+        }
+    }
+
+    @Post('upload/2')
+    public async uploadImage2(@UploadedFile('file') data) {
+        const result = await ImageUploadService.uploadImage({ filename: data.originalname, size: data.size, buffer: data.buffer })
+
+        return {
+            name: data.originalname,
+            status: 'done',
+            url: result.url,
+            thumbUrl: result.url
         }
     }
 
