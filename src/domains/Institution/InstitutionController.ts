c81434e3437faef6381fcb1f95f6a548045c1875
mshlz <mateusholzschuh@hotmail.com>
2022-08-28 19:38:02 -0300

    fix: isntitution permission

--- src/domains/Institution/InstitutionController.ts
+++ src/domains/Institution/InstitutionController.ts
@@ -1,41 +1,43 @@
-import { Body, Delete, Get, JsonController, Param, Post, QueryParams } from 'routing-controllers'
-import { ApiResponse } from '../../interfaces/ApiResponse'
-import { Authorized } from '../../utils/auth'
-import { success } from '../../utils/http/responses'
-import { Validate } from '../../utils/validator/Validator'
-import { institutionService } from './InstitutionService'
-import rules from './validation/rules'
+import { Body, Delete, Get, JsonController, Param, Post, QueryParams } from "routing-controllers";
+import { ApiResponse } from "../../interfaces/ApiResponse";
+import { Authorized, Role } from "../../utils/auth";
+import { success } from "../../utils/http/responses";
+import { Validate } from "../../utils/validator/Validator";
+import { institutionService } from "./InstitutionService";
+import rules from "./validation/rules";
 
-@JsonController('/institutions/')
+@JsonController("/institutions/")
 @Authorized()
 export class InstitutionController {
-    @Get()
-    public async list(@QueryParams() query): Promise<ApiResponse> {
-        const institutions = await institutionService.list(query.page, query.per_page)
+  @Get()
+  public async list(@QueryParams() query): Promise<ApiResponse> {
+    const institutions = await institutionService.list(query.page, query.per_page);
 
-        return institutions
-    }
+    return institutions;
+  }
 
-    @Get(':id')
-    public async getOne(@Param('id') id: string): Promise<ApiResponse> {
-        return success(await institutionService.get(id))
-    }
+  @Get(":id")
+  public async getOne(@Param("id") id: string): Promise<ApiResponse> {
+    return success(await institutionService.get(id));
+  }
 
-    @Post()
-    @Validate(rules.onCreate)
-    public async create(@Body() data: any): Promise<ApiResponse> {
-        return success(await institutionService.create(data))
-    }
+  @Post()
+  @Validate(rules.onCreate)
+  @Authorized(Role.TEACHER)
+  public async create(@Body() data: any): Promise<ApiResponse> {
+    return success(await institutionService.create(data));
+  }
 
-    @Post(':id')
-    @Validate(rules.onUpdate)
-    public async update(@Body() data: any, @Param('id') id: string): Promise<ApiResponse> {
-        return success(await institutionService.update(id, data))
-    }
+  @Post(":id")
+  @Validate(rules.onUpdate)
+  @Authorized(Role.TEACHER)
+  public async update(@Body() data: any, @Param("id") id: string): Promise<ApiResponse> {
+    return success(await institutionService.update(id, data));
+  }
 
-    @Delete(':id')
-    public async delete(@Param('id') id: string): Promise<ApiResponse> {
-        return success(await institutionService.delete(id))
-    }
-
+  @Delete(":id")
+  @Authorized(Role.MODERATOR)
+  public async delete(@Param("id") id: string): Promise<ApiResponse> {
+    return success(await institutionService.delete(id));
+  }
 }
